d8091f406b49c36d7ec6ad25bfb9c5cf19b371bd
denmitchell <den.mitchell@yahoo.com>
2018-07-20 14:07:43 -0400

    Replaced Merge command in SaveTestJson stored procedure due to inconsistent behavior when executing.

--- EDennis.MigrationsExtensions/SaveTestJson.sql
+++ EDennis.MigrationsExtensions/SaveTestJson.sql
@@ -1,4 +1,6 @@
-﻿/****** Object:  StoredProcedure [_maintenance].[SaveTestJson]    Script Date: 6/4/2018 12:15:49 PM ******/
+﻿USE [Form255]
+GO
+/****** Object:  StoredProcedure [_maintenance].[SaveTestJson]    Script Date: 7/20/2018 1:38:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -8,12 +10,30 @@ GO
 -- Create date: 2018-05-24
 -- Description:	Merges a record into 
 --              _maintenance.TestJson
+-- Modified on: 2018-07-20:
+--				Replaced Merge command with
+--				Delete and Insert because the
+--				merge command seemed to be
+--				working inconsistently -- 
+--				sometimes no JSON was inserted
+--				(for some users and not others).
+--				This was a mystery that we 
+--				couldn't totally solve except
+--				by updating this procedure.
 -- =============================================
-CREATE PROCEDURE [_maintenance].[SaveTestJson] 
+ALTER PROCEDURE [_maintenance].[SaveTestJson] 
 	@Project varchar(100), @Class varchar(100), @Method varchar(100), 
 	@FileName varchar(100), @Json varchar(max)
 AS
 BEGIN
+
+	delete from _maintenance.TestJson
+		where Project = @Project and Class = @Class and Method = @Method and FileName = @FileName
+
+	insert into _maintenance.TestJson (Project,Class,Method,FileName,Json)
+				values(@Project, @Class, @Method, @FileName, @Json);
+
+	/*
 
 	merge _maintenance.TestJson as target
 	using (select @Project Project, @Class Class, @Method Method, @FileName FileName, @Json Json) as source
@@ -26,5 +46,5 @@ BEGIN
 		then insert (Project,Class,Method,FileName,Json)
 			values(source.Project, source.Class, source.Method, source.FileName, source.Json);
 
-
+	*/
 END
